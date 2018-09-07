0c6a019308c24584c5f8ac97f3e363ae552eb1c4
khanhafe <khanhafe@gmail.com>
2018-09-07 15:51:36 +0530

    props

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./header.jsx";
+import Content from "./content.jsx";
 class Home extends React.Component {
   render() {
     return (
       <div>
         <Header title="Home page" />
+        <Content myTitle="this is Home Page from props" />
         this is home
       </div>
     );
--- src/components/service.jsx
+++ src/components/service.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./header.jsx";
-
+import Content from "./content.jsx";
 class Service extends React.Component {
   render() {
     return (
       <div>
         <Header title="service page" />
+        <Content myTitle="this is Service Page from props" />
         this is service
       </div>
     );
