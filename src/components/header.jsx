c0994447918c5696812feee3bb41e31165420f7c
khanhafe <khanhafe@gmail.com>
2018-09-11 13:47:56 +0530

    props work with style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+var styles = {
+  color: "red",
+  backgroundColor: "black",
+  fontWeight: "bold",
+  textDecoration: "none",
+  marginLeft: "20px"
+};
+
 class Header extends React.Component {
   render() {
     return (
       <div>
-        <Link to="/">Home</Link>
-        <Link to="/service">Service</Link>
+        <Link to="/">
+          <a href="#" style={styles}>
+            Home
+          </a>
+        </Link>
+        <Link to="/service">
+          <a href="#" style={styles}>
+            Service
+          </a>
+        </Link>
       </div>
     );
   }
